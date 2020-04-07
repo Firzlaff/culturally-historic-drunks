3d5fea753de0267880d53a0a931fcc3582306590
GLX Enigma <tyrussell2112@gmail.com>
2020-04-06 19:39:52 -0600

    added in background

---
+++ script.js
@@ -0,0 +1,7 @@
+function show(){
+    document.getElementsByClassName(hidden).style.visibility = "visible"
+};
+
+function demoDisplay() {
+    document.getElementById("myP1").style.display = "none";
+  }
